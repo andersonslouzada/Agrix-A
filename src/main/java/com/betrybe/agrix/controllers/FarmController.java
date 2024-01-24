2f4681da6ee5399caf44dfc5caba5b12e6f5615f
Anderson Louzada <anderson.s.louzada@gmail.com>
2024-01-24 14:42:33 -0300

    feat: create route get

--- src/main/java/com/betrybe/agrix/controllers/FarmController.java
+++ src/main/java/com/betrybe/agrix/controllers/FarmController.java
@@ -1,12 +1,13 @@
 package com.betrybe.agrix.controllers;
 
 import com.betrybe.agrix.controllers.dto.FarmDto;
-import com.betrybe.agrix.controllers.dto.ResponseDto;
 import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.services.FarmService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,11 @@ public class FarmController {
 
   private final FarmService farmService;
 
+  /**
+   * Instantiates a new Farm controller.
+   *
+   * @param farmService the farm service
+   */
   @Autowired
   public FarmController(FarmService farmService) {
     this.farmService = farmService;
@@ -38,4 +44,15 @@ public class FarmController {
     return ResponseEntity.status(HttpStatus.CREATED).body(FarmDto.toDto(newFarm));
   }
 
+  /**
+   * Gets all farms.
+   *
+   * @return the all farms
+   */
+  @GetMapping
+  public ResponseEntity<List<FarmDto>> getAllFarms() {
+    List<Farm> farms = farmService.getAllFarms();
+    List<FarmDto> farmsDto = farms.stream().map(FarmDto::toDto).toList();
+    return ResponseEntity.ok(farmsDto);
+  }
 }
--- src/main/java/com/betrybe/agrix/services/FarmService.java
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -2,6 +2,7 @@ package com.betrybe.agrix.services;
 
 import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.models.repositories.FarmRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +18,13 @@ public class FarmService {
   public FarmService(FarmRepository farmRepository) {
     this.farmRepository = farmRepository;
   }
-  
+
   public Farm insertFarm(Farm farm) {
     return farmRepository.save(farm);
+  }
+
+  public List<Farm> getAllFarms() {
+    return farmRepository.findAll();
   }
 
 }
